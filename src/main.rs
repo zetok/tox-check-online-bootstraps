a7c425595877d69b41b7070687b05a1fa2f0fd71
Zetok Zalbavar <zetok@openmailbox.org>
2015-08-29 17:19:07 +0100

    Notify via email failed bootstraps

--- src/main.rs
+++ src/main.rs
@@ -38,12 +38,31 @@ use std::io::{Read,Write};
 use std::fs::File;
 
 
+/*
+    For email notifications about offline nodes
+*/
+//use smtp::sender::{Sender, SenderBuilder};
+use smtp::email::EmailBuilder;
+//use smtp::authentication::Mecanism;
+extern crate smtp;
+use smtp::sender::SenderBuilder;
+
+
+
 
 #[derive(Debug)]
 struct Bootstrap {
     ip: String,
     port: u16,
     pubkey: PublicKey,
+}
+
+#[derive(Debug)]
+struct Credentials {
+    login:  String,
+    passwd: String,
+    domain: String,
+    email:  String,
 }
 
 /*
@@ -88,6 +107,50 @@ fn parse_to_bootstrap(string: &str) -> Option<Bootstrap> {
 
 
 
+fn get_credentials(file: &str) -> Option<Credentials> {
+    if let Ok(cred) = vec_strings(file) {
+        if cred.len() == 4 {
+            // assume that those are credentials
+            return Some(Credentials {
+                login:  cred[0].to_string(),
+                passwd: cred[1].to_string(),
+                domain: cred[2].to_string(),
+                email:  cred[3].to_string(),
+            })
+        }
+    }
+    None
+}
+
+
+fn send_mail(creds: Credentials, message: String) {
+    // make an email
+    let mut builder = EmailBuilder::new();
+    builder = builder.to(&*creds.email);
+    let from = format!("{}@{}", creds.login, creds.domain);
+    builder = builder.from(&*from);
+    builder = builder.subject("Failed nodes");
+    builder = builder.body(&message);
+
+    let email = builder.build();
+
+    // Connect to remote server
+    let mut sender = SenderBuilder::new(&*creds.domain).unwrap()
+            // Set the name sent during EHLO/HELO, default is `localhost`
+            .hello_name(&creds.domain)
+            // Add credentials for authentication
+            .credentials(&creds.login, &creds.passwd)
+            .build();
+
+    match sender.send(email) {
+        Ok(_) => {},
+        Err(e) => panic!("Error while sending mail: {}", e),
+    }
+
+}
+
+
+
 fn main() {
 
     let nodes = match vec_strings("nodes_list") {
@@ -106,15 +169,15 @@ fn main() {
 
             // new Tox instance for bootstrapping
             let mut tox = Tox::new(ToxOptions::new(), None).unwrap();
-            print!("Boostrapping from {} ...  ", &n);
+            //print!("Boostrapping from {} ...  ", &n);
             // workaround, since printing line without \n often doesn't want
             // to work correctly
             drop(std::io::stdout().flush());
 
-            if let Err(e) = tox.bootstrap(&node.ip, node.port, node.pubkey) {
+            if let Err(_e) = tox.bootstrap(&node.ip, node.port, node.pubkey) {
                 // break if something supplied won't work
-                println!("BAD BOOTSTRAP - FAILED ✗");
-                println!("ERROR: {:?}", e);
+                //println!("BAD BOOTSTRAP - FAILED ✗");
+                //println!("ERROR: {:?}", e);
                 continue;
             };
 
@@ -127,15 +190,15 @@ fn main() {
                             // and if that's the case, push it to_return
                             match c {
                                 Connection::Tcp => {
-                                    to_return.push((&*n).to_string());
-                                    println!("SUCCESSFUL ✔");
+                                    //to_return.push((&*n).to_string());
+                                    //println!("SUCCESSFUL ✔");
                                     // break out of the loop
                                     pushed = true;
                                     break;
                                 },
                                 Connection::Udp => {
-                                    to_return.push((&*n).to_string());
-                                    println!("SUCCESSFUL ✔");
+                                    //to_return.push((&*n).to_string());
+                                    //println!("SUCCESSFUL ✔");
                                     // break out of the loop
                                     pushed = true;
                                     break;
@@ -148,22 +211,32 @@ fn main() {
                 }
 
                 if current_time + 20 < UTC::now().timestamp() {
-                    // if unable to bootstrap for 30s, break loop
-                    println!("FAILED ✗");
+                    // if unable to bootstrap for 30s, put it into message
+                    // and break loop
+                    //println!("FAILED ✗");
+                    to_return.push((&*n).to_string());
                     break;
                 }
                 tox.wait();
             }
         } else {
             // break if there was no node
+            println!("There was no node!");
             break;
         }
     }
 
-    println!("\n\n\n");
-
-    // print whichever nodes worked
-    for n in to_return {
-        println!("{}", &n);
+    if to_return.len() >= 1 {
+        let mut send_string = String::new();
+        for s in to_return {
+            let push = format!("{}\n", s);
+            send_string.push_str(&push);
+        }
+        // get credentials from file, and if successful, try to send mail
+        let creds_file = ".properties";
+        match get_credentials(creds_file) {
+            Some(p) => send_mail(p, send_string),
+            None    => println!("Failed to read file {}", creds_file),
+        }
     }
 }
